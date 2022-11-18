c120202526ff9f0404b504d630f98cae10b80c45
Artem <a.bendrat2011@yandex.ru>
2022-11-18 20:35:27 +0300

    Work1

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+// See https://aka.ms/new-console-template for more information
+
+System.Console.WriteLine();
 Console.WriteLine("Hello, World!");
+Console.WriteLine("2+2");
+*/
+
+
+/*int a=0,b=0,c=0;//целый
+float f=20.4f;
+double d=3.14;//вещественный
+string s="asdasd";
+char c0='a';
+bool flag=true;
+int переменная1=0;
+
+int @_45f;
+int @int=4;
+*/
+//1.С клавиатуры вводится целое число. Вывести квадрат числа
+
+int a;
+a=10;
+System.Console.Write("Введите число:");
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+int b=a*a;
+System.Console.WriteLine(Convert.ToString(a)+"^2="+b.ToString());
+
---
+++ 002/Program.cs
@@ -0,0 +1,24 @@
+﻿// //*2.С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго (блок-схема)
+
+/*
+//полное условие
+if ()
+{
+    //true
+}
+else
+{
+    //false
+}
+//неполное условие
+if()
+{
+    //true
+}
+*/
+
+double a=Convert.ToDouble(Console.ReadLine());
+double b=Convert.ToDouble(Console.ReadLine());
+
+if (a*a==b) System.Console.WriteLine($"{b} является квадратом {a}");
+if (b*b==b) System.Console.WriteLine($"{a} является квадратом {b}");
