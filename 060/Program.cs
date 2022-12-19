26ab52956a9d0b334035236a00bf91f081cc1556
Artem <a.bendrat2011@yandex.ru>
2022-12-19 19:16:23 +0300

    Lesson6

---
+++ 060/Program.cs
@@ -0,0 +1,22 @@
+﻿// 60.Показать двумерный массив размером m×n заполненный целыми случайными числами
+
+int[,] Create2DArray(int n,int m)
+{
+    int[,] a=new int[n,m];
+    for(int i=0;i<n;i++)
+        for (int j=0;j<m;j++)
+                a[i,j]=i+j;
+    return a;
+}
+
+void Print2DArray(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+        {
+    for(int j=0;j<a.GetLength(1);j++)
+        System.Console.Write($"{a[i,j],4}");
+            System.Console.WriteLine();
+        }
+}
+int[,] a=Create2DArray(4,4);
+Print2DArray(a);
---
+++ 062/Program.cs
@@ -0,0 +1,21 @@
+﻿// 62.В двумерном массиве n×k заменить четные элементы на противоположные по значению 
+//(a[i,j]=-a[i,j]). Написать отдельную подпрограмму на вход которой подается массив, а на выходе измененный массив.
+
+int[,] Create2DArray(int n,int m)
+{
+    int[,] a=new int[n,m];
+    for(int i=0;i<n;i++)
+        for (int j=0;j<m;j++)
+                a[i,j]=i+j;
+    return a;
+}
+
+void Print2DArray(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+        {
+    for(int j=0;j<a.GetLength(1);j++)
+        System.Console.Write($"{a[i,j],4}");
+            System.Console.WriteLine();
+        }
+}
